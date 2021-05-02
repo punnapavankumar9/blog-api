271918cc09db8e6a9c516ac306aa58abb07f43ed
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2021-05-02 23:56:25 +0530

    configured for production

--- blog1/settings.py
+++ blog1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mg$jf$0uao$8v%gw8-+=%$w6m)%4v(-kg+5y-glo9e+!xyrymv'
+SECRET_KEY = os.environ.get('DJANGO_SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
